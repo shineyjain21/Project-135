89730012b792aea53b2270d9ce795ed9accb4cbf
shineyjain21 <103041368+shineyjain21@users.noreply.github.com>
2022-04-19 20:42:15 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 Status = "";
-input_text = "";
+objects = [];
 
 function setup(){
     canvas = createCanvas(300,290);
@@ -10,7 +10,7 @@ function setup(){
 }
 function start(){
     object_Detector = ml5.objectDetector('cocossd',modelLoaded);
-    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+    document.getElementById("status").innerHTML = "Status: Detecting Object";
     input_text = document.getElementById("input_id").value;
 }
 function modelLoaded(){
@@ -19,4 +19,39 @@ function modelLoaded(){
 }
 function draw(){
     image(video,0,0,300,290);
+    if(Status != ""){
+        object_Detector.detect(video, gotResults);
+        for(i = 0;i < objects.length;i++){
+            document.getElementById("status").innerHTML = "Status : Object Detected";
+            console.log(objects.length);
+            fill("#ff0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%",objects[i].x + 15,objects[i].y + 15);
+            noFill();
+            stroke("#ff0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+
+            if(objects[i].label == input_text){
+                video.stop();
+                object_Detector.detect(gotResults);
+                document.getElementById("object_found").innerHTML = input_text+" Found";
+                var synth = window.speechSynthesis;
+                var utterThis = new SpeechSynthesisUtterance(input_text + "Found");
+                synth.speak(utterThis);
+            }
+            else{
+                document.getElementById("object_found").innerHTML = input_text + " Not Found";
+            }
+        }
+    }
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    else{
+        console.log(results);
+        objects = results;
+    }
 }
